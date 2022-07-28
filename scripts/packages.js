daed193bfdcd1d4b50216f59eb36776916d40a3b
fr58386612 <58386612@QQ.COM>
2022-07-28 11:18:57 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -77,22 +77,22 @@ module.exports = [
   //   name: 'luci-app-unblockneteasemusic',
   //   desc: '解锁网易云音乐 (无界面)',
   // },
-  // {
-  //   name: 'luci-app-syncdial',
-  //   desc: '多拨虚拟网卡（需要光猫支持多拨）',
-  // },
-  // {
-  //   name: 'luci-app-passwall',
-  //   desc: 'passwall代理软件',
-  // },
-  // {
-  //   name: 'luci-app-ssr-plus',
-  //   desc: 'ssr-plus 代理软件',
-  // },
-  // {
-  //   name: 'luci-app-aliddns',
-  //   desc: '阿里云ddns插件',
-  // },
+  {
+    name: 'luci-app-syncdial',
+    desc: '多拨虚拟网卡（需要光猫支持多拨）',
+  },
+  {
+    name: 'luci-app-passwall',
+    desc: 'passwall代理软件',
+  },
+//   {
+//     name: 'luci-app-ssr-plus',
+//     desc: 'ssr-plus 代理软件',
+//   },
+  {
+    name: 'luci-app-aliddns',
+    desc: '阿里云ddns插件',
+  },
   // {
   //   name: 'luci-app-ddns',
   //   desc: '动态域名 DDNS',
@@ -101,10 +101,10 @@ module.exports = [
   //   name: 'luci-app-eqos',
   //   desc: '依IP地址限速',
   // },
-  // {
-  //   name: 'luci-app-filetransfer',
-  //   desc: '文件传输',
-  // },
+  {
+    name: 'luci-app-filetransfer',
+    desc: '文件传输',
+  },
   // {
   //   name: 'luci-app-ddnsto',
   //   desc: 'DDNSTO 内网穿透',
@@ -117,10 +117,10 @@ module.exports = [
   //   name: 'luci-app-mwan3helper',
   //   desc: ' MWAN3分流助手',
   // },
-  // {
-  //   name: 'luci-app-wrtbwmon',
-  //   desc: '实时流量监测',
-  // },
+  {
+    name: 'luci-app-wrtbwmon',
+    desc: '实时流量监测',
+  },
   {
     name: 'luci-app-serverchan',
     desc: '微信/Telegram推送',
@@ -133,12 +133,12 @@ module.exports = [
     name: 'luci-app-dailybonus',
     desc: '京东签到服务',
   },
-  // {
-  //   name: 'luci-app-frpc',
-  //   desc: 'frpc 网络代理',
-  // },
-  // {
-  //   name: 'luci-app-frps',
-  //   desc: 'frps 网络代理',
-  // },
+  {
+    name: 'luci-app-frpc',
+    desc: 'frpc 网络代理',
+  },
+  {
+    name: 'luci-app-frps',
+    desc: 'frps 网络代理',
+  },
 ];
